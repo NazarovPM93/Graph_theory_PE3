8ce511c49786bad37ffec3783ff1b5a0f15afa2a
NazarovPM93 <65697785+NazarovPM93@users.noreply.github.com>
2020-06-11 21:36:17 +0700

    Add files via upload

---
+++ Проверка на дерево Моргунов Попков Павлов Назаров/1) тексты программ/Обход в глубину.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+vector<bool> marks;
+
+void dfs(int cur_node, const vector<vector<int>> G)
+{
+    marks[cur_node] = true;
+    for (int i = 0; i < G[cur_node].size(); i++)
+        if (!marks[G[cur_node][i]])
+            dfs(G[cur_node][i], G);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    ifstream fin("test.txt");
+    ofstream fout("res.txt");
+
+    int n, m;
+    fin >> n >> m;
+    bool result = true;
+    if (m != n - 1)
+    {
+        fout << "0";
+        return 0;
+    }
+
+    vector<vector<int>> Graph(n);
+    int a, b;
+
+    for (int i = 0; i < m; i++)
+    {
+        fin >> a >> b;
+        Graph[a - 1].push_back(b - 1);
+        Graph[b - 1].push_back(a - 1);
+    }
+
+    marks.assign(n, false);
+    dfs(0, Graph);
+
+    for (int i = 0; i < n; i++)
+        if (!marks[i])
+            result = false;
+
+    fout << result;
+    return 0;
+}
---
+++ Проверка на дерево Моргунов Попков Павлов Назаров/1) тексты программ/Обход в ширину.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+vector<bool> marks;
+
+void bfs(int n, const vector<vector<int>> G)
+{
+    queue <int> q;
+    q.push(0);
+    marks[0] = true;
+
+    while (!q.empty())
+    {
+        int cur_node = q.front();
+        q.pop();
+        for (int i : G[cur_node])
+            if (!marks[i])
+            {
+                q.push(i);
+                marks[i] = true;
+            }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    ifstream fin("test.txt");
+    ofstream fout("res.txt");
+
+    int n, m;
+    fin >> n >> m;
+    bool result = true;
+    if (m != n - 1)
+    {
+        fout << "0";
+        return 0;
+    }
+
+    vector<vector<int>> Graph(n);
+    int a, b;
+
+    for (int i = 0; i < m; i++)
+    {
+        fin >> a >> b;
+        Graph[a - 1].push_back(b - 1);
+        Graph[b - 1].push_back(a - 1);
+    }
+
+    marks.assign(n, false);
+    bfs(0, Graph);
+
+    for (int i = 0; i < n; i++)
+        if (!marks[i])
+            result = false;
+
+    fout << result;
+    return 0;
+}
